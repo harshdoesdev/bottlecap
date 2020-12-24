4dc2d205ef6c7003ea7225682b5e28195b85dc28
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-12-25 00:19:24 +0530

    Create preload-image.js

---
+++ examples/preloading/preload-image.js
@@ -0,0 +1,18 @@
+import { ready } from './bottlecap/dom.js';
+import { load_image, load_images } from './bottlecap/loader.js';
+
+const app = async () => {
+
+  const res = './resources/images/';
+  
+  // Load Single Image / Sprite
+
+  const img1 = await load_image(res + 'img1.png'); // returns new Image
+  
+  // Load Multiple Images and Sprites
+  
+  const imgs = await load_images(res + './img2.png', res + './img3.png', res + './img4.png'); // returns Array of Image Objects
+
+};
+
+ready(app);
