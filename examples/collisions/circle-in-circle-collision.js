fb487889355a76819382c51298dcd378645a13a1
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-12-30 23:49:21 +0530

    Update circle-in-circle-collision.js

--- examples/collisions/circle-in-circle-collision.js
+++ examples/collisions/circle-in-circle-collision.js
@@ -1,6 +1,6 @@
-import { ready, on } from '../../bottlecap/dom.js';
-import { createCanvas, clearCanvas, screenToCanvas } from '../../bottlecap/canvas.js';
-import { circleInCircle } from '../../bottlecap/collisions.js';
+import { ready, on } from '../../src/dom.js';
+import { createCanvas, clearCanvas, screenToCanvas } from '../../src/canvas.js';
+import { circleInCircle } from '../../src/collisions.js';
 
 const app = () => {
 
