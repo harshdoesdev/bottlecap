ee0227db9cd97b357f057fd2db6ca9b5e6ff0665
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-12-31 14:03:34 +0530

    Create basic-sprite-animation2.js

---
+++ examples/animations/basic-sprite-animation2.js
@@ -0,0 +1,47 @@
+import { ready, on } from './bottlecap/dom.js';
+import { createCanvas, clearCanvas, screenToCanvas } from './bottlecap/canvas.js';
+import createSprite from './bottlecap/sprite.js';
+
+const app = async () => {
+
+  const { cnv, ctx } = createCanvas();
+
+  document.body.appendChild(cnv);
+
+  // preload spritesheet
+  
+  const playerSpritesheet = await load_image('./resources/player-spritesheet.png');
+  
+  // create sprite object
+  
+  const playerSprite = createSprite(playerSpritesheet, 6, 1, 100);
+  
+  // sprite animations table
+
+  const playerAnimations = {
+  
+    walk: { frameStart: 0, frameEnd: 6 }
+  
+  };
+  
+  // entities
+  
+  const player = { x: 0, y: 0, w: 32, h: 32 };
+
+  const gameloop = () => {
+
+    requestAnimationFrame(gameloop);
+
+    clearCanvas(ctx);
+    
+    playerSprite.setAnimation(playerAnimations.walk);
+    
+    playerSprite.draw(ctx, player.x, player.y, player.w, player.h);
+  
+  };
+  
+  requestAnimationFrame(gameloop);
+
+};
+
+ready(app);
