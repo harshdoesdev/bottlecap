7ae23714ee76ba759ab08716aebdf0158691cf61
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-01-12 23:41:08 +0530

    Update canvas.js
    screenToCanvas fn was transferred to utility.js

--- src/canvas.js
+++ src/canvas.js
@@ -1,7 +1,6 @@
 export const createCanvas = (width = 256, height = 256, bg = '#000') => {
 
-  const cnv = document.createElement('canvas'),
-        ctx = cnv.getContext('2d');
+  const cnv = document.createElement('canvas'), ctx = cnv.getContext('2d');
 
   cnv.width = width;
 
@@ -9,32 +8,18 @@ export const createCanvas = (width = 256, height = 256, bg = '#000') => {
 
   cnv.style.background = bg;
 
-  return { cnv, ctx };
-
-};
-
-export const clearCanvas = ctx => {
-  
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-};
-
-// converts screen point to canvas point
-
-export const screenToCanvas = (cnv, x, y) => {
-
-  const rect = cnv.getBoundingClientRect(), // abs. size of element
-        
-        scaleX = cnv.width / rect.width,    // relationship bitmap vs. element for X
-        
-        scaleY = cnv.height / rect.height;  // relationship bitmap vs. element for Y
-
-  return {
+  return { 
     
-    x: (x - rect.left) * scaleX,   // scale mouse coordinates after they have
-    
-    y: (y - rect.top) * scaleY     // been adjusted to be relative to element
+    cnv, // canvas element
+
+    ctx, // 2d context
+
+    clearCanvas () {
+
+      ctx.clearRect(0, 0, cnv.width, cnv.height);
+
+    }
   
-  }
+  };
 
 };
