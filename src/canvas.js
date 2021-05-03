795b7a0a3f2250e838dca13b0fd30aff3531ebea
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-05-03 22:47:36 +0530

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -1,21 +1,21 @@
-export const createCanvas = (width, height, bg = '#000') => {
+export const createCanvas = ({ width = 400, height = 400, background = 'black' } = {}) => {
 
-  const cnv = document.createElement('canvas'), ctx = cnv.getContext('2d');
+    const cnv = document.createElement('canvas'), ctx = cnv.getContext('2d'); // canvas and context
 
-  cnv.width = width || window.innerWidth;
+    cnv.width = width; // set width
 
-  cnv.height = height || window.innerHeight;
+    cnv.height = height; // set height
 
-  cnv.style.background = bg;
+    cnv.style.background = background; // change background style using css
 
-  return { 
+    return { 
+      
+      cnv, // canvas element
+      
+      ctx, // 2d context
+
+      clearCanvas: () => ctx.clearRect(0, 0, cnv.width, cnv.height)
     
-    cnv, // canvas element
-
-    ctx, // 2d context
-
-    clearCanvas: () => ctx.clearRect(0, 0, cnv.width, cnv.height)
-  
-  };
+    };
 
 };
