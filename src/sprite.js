834c968cf95ffd4ca29d2752904ba3be3e173194
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-05-12 17:44:56 +0530

    Totally new API for sprite and animation
    More powerful Sprite Animations

--- src/sprite.js
+++ src/sprite.js
@@ -1,69 +1,245 @@
-const createSprite = (spritesheet, numCol, numRow, delay) => {
+import Signal from "./signal.js";
 
-  const frameWidth = spritesheet.width / numCol, frameHeight = spritesheet.height / numRow;
+export class Animation {
 
-  const maxFrames = numCol * numRow - 1;
-
-  let currentFrame = 0, then;
-
-  let currentAnimation = { frameStart: 0 }, prevAnimation;
-
-  return {
-
-    setAnimation(animation) {
-
-      if(animation !== prevAnimation) return;
-
-      prevAnimation = currentAnimation; 
+    constructor({ name = '', frames, firstStep = 0, lastStep, delay = 100, autoStart = true }) {
         
-      currentAnimation = animation;
+        this.sprite = null;
 
-      currentFrame = currentAnimation.frameStart;
+        if(!frames) {
 
-    },
-
-    draw(ctx, x, y, w = frameWidth, h = frameHeight, flipX = false, flipY = false) {
-
-      const { frameEnd = maxFrames, frameStart } = currentAnimation;
-
-      const now = performance.now();
-
-      if(!then || (now - then) >= delay) {
-
-        currentFrame++;
-
-        if(currentFrame > frameEnd) {
-          
-          currentFrame = frameStart;
-
+            this.firstStep = firstStep;
+            this.lastStep = lastStep;
+        
+        } else {
+        
+            this.frames = frames;
+            this.lastStep = this.frames.length - 1;
+            this.firstStep = 0;
+        
         }
 
-        then = now;
+        this.name = name;
+        this.delay = delay;
+        this.currentStep = this.firstStep;
+        this.autoStart = autoStart;
+        this.hasStarted = false;
+        this.isRunning = false;
+    
+    }
 
-      }
-
-      const col = currentFrame % numCol, row = Math.floor(currentFrame / numCol);
-
-      ctx.save();
-      
-      if(flipX || flipY) {
-
-        ctx.translate(x + w / 2, y + w / 2);
+    start() {
         
-        ctx.scale(flipX ? -1 : 1, flipY ? -1 : 1);
-      
-        ctx.translate(-(x + w / 2), -(y + w / 2));
+        if(!this.hasStarted) {
+        
+            this.hasStarted = true;
+            this.isRunning = true;
+            this.sprite.onAnimationStart.emit(this.name);
+        
+        }
+    
+    }
 
-      }
+    pause() {
+    
+        if(this.isRunning && this.hasStarted) {
+    
+            this.isRunning = false;
+    
+        }
+    
+    }
 
-      ctx.drawImage(spritesheet, col * frameWidth, row * frameHeight, frameWidth, frameHeight, x, y, w, h);
+    stop() {
+    
+        if(this.hasStarted) {
+    
+            this.hasStarted = false;
+            this.isRunning = false;
+            this.currentStep = this.firstStep;
+            this.sprite.onAnimationStop.emit(this.name);
+    
+        }
+    
+    }
 
-      ctx.restore();
+    next() {
+    
+        let currFrame;
+    
+        if(this.isRunning) {
+    
+            this.currentStep++;
+    
+            if(this.currentStep > this.lastStep) {
+    
+                this.currentStep = this.firstStep;
+                this.sprite.onAnimationEnd.emit(this.name);
+    
+            }
+    
+            currFrame = !this.frames ? this.currentStep : this.frames[this.currentStep];
+    
+        }
+    
+        return currFrame;
+    
+    }
+
+}
+
+export class AnimatedSprite {
+
+    onAnimationStart = new Signal()
+
+    onAnimationEnd = new Signal()
+
+    onAnimationStop = new Signal()
+
+    constructor({ spritesheet, columns, rows, frameWidth, frameHeight, width, height }) {
+
+        this.spritesheet = spritesheet;
+        
+        this.numCol = columns;
+        this.numRow = rows;
+
+        this.frameWidth = frameWidth || this.spritesheet.width / this.numCol;
+        this.frameHeight = frameHeight || this.spritesheet.height / this.numRow;
+
+        this.width = width || this.frameWidth;
+        this.height = height || this.frameHeight;
+
+        this.maxFrames = this.numCol * this.numRow - 1;
+
+        this.animations = new Map;
+        this.currentFrame = null;
+        this.currentAnimation = null;
+
+        this.flipX = false;
+        this.flipY = false;
+
+        this.rotation = 0;
+
+        this.then = null;
+    
+    }
+
+    draw(ctx, x, y) {
+
+        if(!this.currentAnimation) throw new Error(
+
+            `Can't Draw AnimatedSprite. No Animation has been set.`
+        
+        );
+
+        const now = performance.now();
+  
+        if(!this.then || (now - this.then) >= this.currentAnimation.delay) {
+
+            this.currentFrame = this.currentAnimation.next();
+    
+            this.then = now;
+  
+        }
+
+        const col = this.currentFrame % this.numCol;
+
+        const row = Math.floor(this.currentFrame / this.numCol);
+
+        ctx.save();
+
+        if(this.flipX || this.flipY || this.rotation > 0) {
+  
+            ctx.translate(x + this.width / 2, y + this.width / 2);
+            
+            ctx.rotate(this.rotation * Math.PI / 180);
+
+            ctx.scale(
+                this.flipX ? -1 : 1, 
+                this.flipY ? -1 : 1
+            );
+        
+            ctx.translate(-(x + this.width / 2), -(y + this.width / 2));
+  
+        }
+  
+        ctx.drawImage(
+            
+            this.spritesheet, 
+            
+            col * this.frameWidth, 
+            
+            row * this.frameHeight, 
+            
+            this.frameWidth, 
+            
+            this.frameHeight, 
+            
+            x, 
+            
+            y, 
+            
+            this.width, 
+            
+            this.height
+        
+        );
+  
+        ctx.restore();
 
     }
 
-  }
 
-};
+    addAnimation(name, animation) {
 
-export default createSprite;
+        if(this.animations.has(name)) throw new Error(
+
+            `Animation with name ${name} already exists`
+        
+        );
+        
+        animation.name = name;
+    
+        animation.sprite = this;
+
+        if(animation.lastStep == null) {
+            animation.lastStep = this.maxFrames;
+        }
+    
+        this.animations.set(name, animation);
+    
+        if(!this.currentAnimation) {
+            this.setAnimation(name);
+        }
+    
+    }
+
+    setAnimation(animationName) {
+
+        if(!this.animations.has(animationName)) throw new Error(
+
+            `Animation with name ${animationName} does not exists.`
+        
+        )
+
+        if(this.currentAnimation && animationName === this.currentAnimation.name) return;
+
+        this.stopAnimation();
+
+        this.currentAnimation = this.animations.get(animationName);
+
+        if(this.currentAnimation.autoStart) {
+            this.currentAnimation.start();
+        }
+
+        this.then = null;
+
+    }
+
+    stopAnimation() {
+    
+        this.currentAnimation?.stop();
+    
+    }
+
+}
