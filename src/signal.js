61cf013dfe83a0aa3126d240e28044897696bd65
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-05-12 17:39:39 +0530

    Create signal.js

---
+++ src/signal.js
@@ -0,0 +1,21 @@
+export default class Signal {
+
+    constructor() {
+        this.listeners = new Set;
+    }
+
+    add(fn) {
+        if(typeof fn !== 'function') throw new Error('Signal: Listener must be a function.');
+        this.listeners.add(fn);
+        return () => this.remove(fn);
+    }
+
+    remove(fn) {
+        this.listeners.remove(fn);
+    }
+
+    emit(...args) {
+        this.listeners.forEach(fn => fn(...args));
+    }
+
+}
