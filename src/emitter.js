798055dee7c353c60ab31913e760a605e5453290
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-05-13 17:52:14 +0530

    Create emitter.js

---
+++ src/emitter.js
@@ -0,0 +1,43 @@
+export default class EventEmitter {
+
+    constructor() {
+        
+        this.events = new Map;
+    
+    }
+
+    on(type, listener) {
+        
+        if(!this.events.has(type)) {
+            this.events.set(type, new Set);
+        }
+        
+        const listeners = this.events.get(type);
+        
+        listeners.add(listener);
+    
+    }
+
+    off(type, listener) {
+
+        const listeners = this.events.get(type);
+
+        if(!listeners) return;
+        
+        listeners.delete(listener);
+        
+        if(!listeners.size) this.events.delete(type);
+    
+    }
+
+    emit(type, ...args) {
+
+        const listeners = this.events.get(type);
+
+        if(!listeners) return;
+
+        listeners.forEach(listener => listener(...args));
+
+    }
+
+}
