7827d98e5ca3953f5830c382d96a4a2b108743c6
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-05-28 10:29:50 +0530

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,5 +1,11 @@
 const audioCtx = new AudioContext();
 
+export const ASSET_TYPE_IMAGE = 'image';
+
+export const ASSET_TYPE_AUDIO = 'audio';
+
+export const ASSET_TYPE_JSON = 'json';
+
 export const loadImage = (name, src) => {
 
     return new Promise((resolve, reject) => {
@@ -8,7 +14,7 @@ export const loadImage = (name, src) => {
 
         img.crossOrigin = 'Anonymous';
 
-        img.onload = () => resolve({ type: 'image', name, value: img });
+        img.onload = () => resolve({ type: ASSET_TYPE_IMAGE, name, value: img });
 
         img.onerror = () => reject(new Error(`Couldn't load Image: ${src}`));
 
@@ -29,7 +35,7 @@ export const loadAudio = async (name, src) => {
 
     const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
 
-    return { type: 'audio', name, value: audioBuffer };
+    return { type: ASSET_TYPE_AUDIO, name, value: audioBuffer };
 
 };
 
@@ -42,6 +48,6 @@ export const loadJSON = async (name, src) => {
 
     const json = await res.json();
 
-    return { type: 'json', name, value: json };
+    return { type: ASSET_TYPE_JSON, name, value: json };
 
 };
