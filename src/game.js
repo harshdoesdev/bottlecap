709bc680819b1b40f15887f88ef8a0c83ea920e4
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-05-29 13:07:24 +0530

    Update and rename app.js to game.js

---
+++ src/game.js
@@ -0,0 +1,37 @@
+export default class Game {
+
+    run() {
+        if(this.running) 
+            return;
+        this.running = true;
+        this.init();
+        const loop = () => {
+            this.step();
+            this.lastStep = new Date();
+            this.frameRequest = requestAnimationFrame(loop);
+        }
+        requestAnimationFrame(loop);
+    }
+
+    stop() {
+        if(this.frameRequest)
+            cancelAnimationFrame(this.frameRequest);
+        this.frameRequest = null;
+        this.running = false;
+    }
+
+    step() {
+        const now = new Date;
+        const dt = (now - this.lastStep) / 1000;
+        this.lastStep = now;
+        this.update(dt);
+        this.render();
+    }
+
+    init() {}
+
+    update(dt) {}
+
+    render() {}
+
+}
