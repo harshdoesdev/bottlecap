e5b4fe3af86a3e2d2db1990148269843c21cf0b0
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-05-29 13:38:04 +0530

    Update sound.js

--- src/sound.js
+++ src/sound.js
@@ -1,1 +1,15 @@
 export const audioCtx = new AudioContext();
+
+export const soundMixer = audioCtx.createGainNode();
+
+soundMixer.connect(audioCtx.destination);
+
+export const playSound = (sound, time) => {
+  const source = audioCtx.createBufferSource();
+  source.buffer = sound;
+  source.connect(soundMixer);
+  source.noteOn(time);
+  return source;
+};
+
+export const setVolume = v => soundMixer.gain.value = v;
