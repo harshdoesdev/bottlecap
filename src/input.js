1c82174485cc87a088b5a8984248ff9934cec05f
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-09-01 11:10:18 +0530

    Update input.js

--- src/input.js
+++ src/input.js
@@ -19,36 +19,50 @@ export const KEYS = {
 
 const KEYSTATE = {};
 
+/*
+ * check if a key is down
+ * @param {} key
+ */
+
 export const isKeyDown = key => KEYSTATE[key];
 
+/*
+ * get direction for movement of player
+ */
+
 export const getDirection = () => {
+  
   const DIRECTION = {
     x: 0,
     y: 0
   };
   
-  if(isKeyDown(KEYS.LEFT))
+  if(isKeyDown(KEYS.LEFT) || isKeyDown(KEYS.A))
     DIRECTION.x = -1
-  else if(isKeyDown(KEYS.RIGHT))
+  else if(isKeyDown(KEYS.RIGHT) || isKeyDown(KEYS.D))
     DIRECTION.x = 1;
-  if(isKeyDown(KEYS.UP)) 
+  
+  if(isKeyDown(KEYS.UP) || isKeyDown(KEYS.W)) 
     DIRECTION.y = -1;
-  else if(isKeyDown(KEYS.DOWN))
+  else if(isKeyDown(KEYS.DOWN) || isKeyDown(KEYS.S))
     DIRECTION.y = 1;
   
   return DIRECTION;
+
 };
 
 const handleKeyDown = e => {
+
   if(e.defaultPrevented)
     return;
+  
   KEYSTATE[e.key] = true;
+  
   e.preventDefault();
+
 };
 
-const handleKeyUp = e => {
-  KEYSTATE[e.key] = false;
-};
+const handleKeyUp = e => KEYSTATE[e.key] = false;
 
 on(window, 'keydown', handleKeyDown);
 on(window, 'keyup', handleKeyUp);
