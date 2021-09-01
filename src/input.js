6a4ce75ed07e5e46fe45bee30b98145aff99eeed
rwbeast <64768386+rwbeast@users.noreply.github.com>
2021-09-01 10:40:52 +0530

    Create input.js

---
+++ src/input.js
@@ -0,0 +1,32 @@
+import { on } from './dom.js';
+
+const KEYBOARD = {
+  LEFT: 'ArrowLeft',
+  RIGHT: 'ArrowRight',
+  UP: 'ArrowUp',
+  DOWN: 'ArrowDown',
+  SPACEBAR: ' ',
+  ESCAPE: 'ESCAPE',
+  W: 'W',
+  A: 'A',
+  S: 'S',
+  D: 'D'
+};
+
+const KEYSTATE = {};
+
+export const isKeyDown = key => KEYSTATE[key];
+
+const handleKeyDown = e => {
+  if(e.defaultPrevented)
+    return;
+  KEYSTATE[e.key] = true;
+  e.preventDefault();
+};
+
+const handleKeyUp = e => {
+  KEYSTATE[e.key] = false;
+};
+
+on(window, 'keydown', handleKeyDown);
+on(window, 'keyup', handleKeyUp);
