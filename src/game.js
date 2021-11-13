89444da0898af7f875038d2dd556800da9782780
Harsh Singh <64768386+harshsinghdev@users.noreply.github.com>
2021-11-13 21:26:30 +0530

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,6 +1,3 @@
-import { createCanvas } from './canvas.js';
-import { Camera } from './camera.js'; 
-
 /**
  * Main class, representing the current Game state
  */
@@ -10,39 +7,43 @@ export default class Game {
      * Kickstart the game
      */
     run() {
+        
         if(this.running) 
             return;
+        
         this.running = true;
-        const config = this.constructor.config;
-        if(config) {
-            console.log('Configuring game');
-            console.log('Configuring canvas');
-            const { canvas: { width, height, background }, camera = {} } = config;
-            
-            const canvas = createCanvas(width, height, background);
-            
-            console.log('Configuring camera');
-            this.camera = new Camera(canvas.ctx, camera.x, camera.y, camera.dx, camera.dy);
-            Object.assign(this, canvas);
+        this.loadingAssets = false;
 
-            console.log('Configuration complete');
-            
-            if(config.rootEl) {
-                const rootEl = typeof config.rootEl === 'string' 
-                    ? document.querySelector(config.rootEl) 
-                    : config.rootEl;
-
-                rootEl.appendChild(this.cnv);
-            }
-        }
         this.init();
+        
+        const loadPromises = this.load();
+
+        if(!!loadPromises && Array.isArray(loadPromises) && loadPromises.length) {
+            this.loadingAssets = true;
+            Promise.all(loadPromises).then(loadedAssets => {
+                this.assets = {
+                    image: {},
+                    sound: {},
+                    json: {}
+                };
+                loadedAssets.forEach(({ name, value, type }) => {
+                    this.assets[type][name] = value;
+                });
+                this.loadingAssets = false;
+                this.onLoadComplete();
+            });
+        }
+        
         this.lastStep = new Date();
+    
         const loop = () => {
             this.step();
             this.lastStep = new Date();
             this.frameRequest = requestAnimationFrame(loop);
         }
+    
         requestAnimationFrame(loop);
+    
     }
 
     stop() {
@@ -70,6 +71,12 @@ export default class Game {
     init() {
         console.log('Game Initialized');
     }
+
+    // For Loading stuff
+
+    load() {}
+
+    onLoadComplete() {}
 
     /**
      * Called on each frame to update game states.
