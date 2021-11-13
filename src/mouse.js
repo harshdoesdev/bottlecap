37501f24c8b59257331f7f65e374c50810ace0e4
Harsh Singh <64768386+harshsinghdev@users.noreply.github.com>
2021-11-13 20:16:09 +0530

    Update mouse.js

--- src/mouse.js
+++ src/mouse.js
@@ -6,6 +6,28 @@ export const Mouse = {
     isDown: false
 };
 
+/**
+ * Convert screen coordinates into canvas coordinates.
+ * example: const adjMouse = screenToCanvas(cnv, Mouse.x, Mouse.y); // { x, y }
+ */
+export const screenToCanvas = (cnv, x, y) => {
+
+  const rect = cnv.getBoundingClientRect(), // abs. size of element
+        
+        scaleX = cnv.width / rect.width,    // relationship bitmap vs. element for X
+        
+        scaleY = cnv.height / rect.height;  // relationship bitmap vs. element for Y
+
+  return {
+    
+    x: (x - rect.left) * scaleX,   // scale mouse coordinates after they have
+    
+    y: (y - rect.top) * scaleY     // been adjusted to be relative to element
+  
+  }
+
+};
+
 const handleMouse = ({ clientX, clientY, type }) => {
     Mouse.x = clientX;
     Mouse.y = clientY;
