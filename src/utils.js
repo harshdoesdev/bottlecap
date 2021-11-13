82d92b51d3e8797ad51446f77531d50ae7b53aaa
Harsh Singh <64768386+harshsinghdev@users.noreply.github.com>
2021-11-13 20:14:53 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -53,26 +53,3 @@ export const chunk = (arr, chunkSize) => {
   return chunks;
 
 };
-
-// other stuff
-
-/**
- * Convert screen coordinates into canvas coordinates.
- */
-export const screenToCanvas = (cnv, x, y) => {
-
-  const rect = cnv.getBoundingClientRect(), // abs. size of element
-        
-        scaleX = cnv.width / rect.width,    // relationship bitmap vs. element for X
-        
-        scaleY = cnv.height / rect.height;  // relationship bitmap vs. element for Y
-
-  return {
-    
-    x: (x - rect.left) * scaleX,   // scale mouse coordinates after they have
-    
-    y: (y - rect.top) * scaleY     // been adjusted to be relative to element
-  
-  }
-
-};
