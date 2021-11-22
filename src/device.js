7009fdef4307e0abb1922abe43389ba99ca6e7a0
Harsh Singh <64768386+harshsinghdev@users.noreply.github.com>
2021-11-22 21:48:25 +0530

    Update device.js

--- src/device.js
+++ src/device.js
@@ -4,3 +4,5 @@
  * @return {boolean} true if the device have touch screen capabilities
  */
 export const isTouchscreen = () => 'ontouchstart' in document.documentElement;
+
+export const gamepadAvailable = () => !!(navigator && navigator.getGamepads);
