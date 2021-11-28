209f5bc4198463fcc5f185062cb1c910ae7f55de
Harsh Singh <64768386+harshsinghdev@users.noreply.github.com>
2021-11-28 18:43:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const main = () => {
+function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
 
-  console.log("hello world");
+const correctViewportHeight = debounce(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}, 100);
 
-};
+correctViewportHeight();
 
-window.addEventListener('DOMContentLoaded', main);
+window.addEventListener('resize', correctViewportHeight);
